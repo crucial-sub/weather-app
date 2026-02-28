7350cd20cacad978de5ea61432205403a25ff216
crucial-sub <jssub940@gmail.com>
2026-02-28 13:04:00 +0900

    프로젝트 초기 설정 및 shadcn/ui 구성

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,27 @@
+// shadcn/ui 컴포넌트 통합 익스포트
+export { Button, buttonVariants } from './button'
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from './card'
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from './dialog'
+export { Input } from './input'
+export { ScrollArea, ScrollBar } from './scroll-area'
+export { Skeleton } from './skeleton'
+export { Toaster } from './sonner'
